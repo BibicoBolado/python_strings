e226c0c3f3319745dbcdc3271bd871e522492700
bibicobolado <rodrigosiqueira@id.uff.br>
2019-03-08 14:36:58 -0300

    aula2

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from extratorArgumentosUrl import ExtratorArgumentosUrl
 '''
-argumento = "www.bytebank.com.br/cambio?moedaorigem=real"
+url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar&valor=700"
+
+argumento = "Rodrigo de Oliveira Siqueira"
 #            0123456789 11  15
-substring=argumento[5:11]
-print(substring)
+listaUrl = argumento.split(" ")
+print(listaUrl)
 '''
 
-argumento = "moedaorigem=real"
-#            0123456789 11  15
-listaargumentos = argumento.split("=")
-print(listaargumentos)
+url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar"
+#                   find
+
+argumentosUrl = ExtratorArgumentosUrl(url)
+moedaOrigem,moedaDestino = argumentosUrl.extraiArgumentos()
+print(moedaDestino,moedaOrigem)
+
+#index = url.find("moedadestino") + len("moedadestino") + 1
+#substring = url[index:]
+#print(substring)
