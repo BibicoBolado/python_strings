6a742069007dafd583edc15295241c1a48ab28de
bibicobolado <rodrigosiqueira@id.uff.br>
2019-03-08 14:38:44 -0300

    aula3

--- extratorArgumentosUrl.py
+++ extratorArgumentosUrl.py
@@ -1,30 +1,49 @@
 class ExtratorArgumentosUrl:
     def __init__(self,url):
         if self.urlEhValida(url):
-            self.url = url
+            self.url = url.lower()
         else:
             raise LookupError("Url inválida!!!")
     @staticmethod
     def urlEhValida(url):
-        if url:
+        if url and url.startswith("https://bytebank.com"):
             return True
         else:
             return False
 
     def extraiArgumentos(self):
 
-        buscaMoedaOrigem  = "moedaorigem"
-        buscaMoedaDestino = "moedadestino"
+        buscaMoedaOrigem  = "moedaorigem=".lower()
+        buscaMoedaDestino = "moedadestino=".lower()
 
-        indiceInicialMoedaDestino   = self.encontraIndiceInicial(buscaMoedaDestino)
+
 
         indiceInicialMoedaOrigem    = self.encontraIndiceInicial(buscaMoedaOrigem)
         indiceiFinalMoedaOrigem     = self.url.find("&")
 
         moedaOrigem = self.url[indiceInicialMoedaOrigem:indiceiFinalMoedaOrigem]
-        moedaDestino= self.url[indiceInicialMoedaDestino:]
+
+        if moedaOrigem == "moedadestino":
+            self.trocaMoedaOrigem()
+            indiceInicialMoedaOrigem = self.encontraIndiceInicial(buscaMoedaOrigem)
+            indiceiFinalMoedaOrigem = self.url.find("&")
+            moedaOrigem = self.url[indiceInicialMoedaOrigem:indiceiFinalMoedaOrigem]
+
+        indiceInicialMoedaDestino = self.encontraIndiceInicial(buscaMoedaDestino)
+        indiceFinalMoedaDestino   = self.url.find("&valor")
+        moedaDestino= self.url[indiceInicialMoedaDestino:indiceFinalMoedaDestino]
 
         return moedaOrigem,moedaDestino
 
     def encontraIndiceInicial(self,moedaBuscada):
-        return self.url.find(moedaBuscada) + len(moedaBuscada) + 1
+        return self.url.find(moedaBuscada) + len(moedaBuscada)
+
+    def trocaMoedaOrigem(self):
+        self.url = self.url.replace("moedadestino","real",1)
+        print(self.url)
+
+    def extraiValor(self):
+        buscaValor = "valor="
+        indiceInicialValor = self.encontraIndiceInicial(buscaValor)
+        valor = self.url[indiceInicialValor:]
+        return valor
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from extratorArgumentosUrl import ExtratorArgumentosUrl
 '''
-url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar&valor=700"
+url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar"
 
 argumento = "Rodrigo de Oliveira Siqueira"
 #            0123456789 11  15
@@ -8,13 +8,29 @@ listaUrl = argumento.split(" ")
 print(listaUrl)
 '''
 
-url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar"
+url = "https://bitebank.com/cambio?moedaoRigem=moedadestino&moedadestino=dolar&valor=1500"
 #                   find
 
 argumentosUrl = ExtratorArgumentosUrl(url)
 moedaOrigem,moedaDestino = argumentosUrl.extraiArgumentos()
-print(moedaDestino,moedaOrigem)
+valor = argumentosUrl.extraiValor()
+print(moedaDestino,moedaOrigem,valor)
 
 #index = url.find("moedadestino") + len("moedadestino") + 1
 #substring = url[index:]
 #print(substring)
+
+#string = "bytebankbytebyte"
+#stringNova = string.replace("byte","rodrigo",1)
+#print(stringNova)
+
+banco1 = "bytebank"
+banco2 = "Bytebank".lower()
+
+#print(banco2)
+urlByteBank = "https://bytebank.com"
+url1        = "hhtps://buscasites.com/busca?q=https://bytebank.com"
+url2        = "https://bitebank.com.br"
+url3        = "https://bytebank.com/cambio/teste/teste"
+
+print(url1.startswith(urlByteBank))
